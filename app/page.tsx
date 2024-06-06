b66ac3f5a8ea7348360a3b4bbe16635c42e1ba23
shalri <fsaonlinework777@gmail.com>
2024-06-06 18:07:54 +0800

    Mobile Layout with UI states

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       {/* <main className="flex flex-grow flex-col items-center justify-center p-24"> */}
-      <main>
+      <main className="mx-4 my-20">
         <Dashboard username="Jeremy Robson" />
       </main>
     </>
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -21,7 +21,7 @@ export default function Dashboard({ username }: DashboardProps) {
     <div className="text-white">
       <section className="mb-6 overflow-hidden rounded-[1.10rem] bg-tt-dark-blue">
         <div className="flex items-center justify-center gap-x-4 rounded-b-[1.10rem] bg-tt-blue px-[30px] py-[32px]">
-          <div className="relative h-[72px] w-[72px] rounded-full border-[3px] border-white">
+          <div className="relative h-[70px] w-[70px] rounded-full border-[3px] border-white">
             <Image
               src={user?.image as string | StaticImport}
               fill
@@ -29,18 +29,18 @@ export default function Dashboard({ username }: DashboardProps) {
               alt="user avatar"
             />
           </div>
-          <div className="">
+          <div>
             <p className="text-sm text-tt-pale-blue">Report for</p>
             <h1 className="text-2xl font-semibold text-white/80">
               {user?.name}
             </h1>
           </div>
         </div>
-        <nav className="flex justify-between gap-2 bg-tt-dark-blue px-7 py-6">
+        <nav className="flex justify-between gap-2 bg-tt-dark-blue px-7 pb-6 pt-5">
           <div>
             <button
               className={cn(
-                "text-white/40",
+                "text-white/40 transition-all duration-300 hover:text-white/80",
                 timeframe === "daily" ? "text-white" : "",
               )}
               onClick={() => setTimeframe("daily")}
@@ -51,7 +51,7 @@ export default function Dashboard({ username }: DashboardProps) {
           <div>
             <button
               className={cn(
-                "text-white/40",
+                "text-white/40 transition-all duration-300 hover:text-white/80",
                 timeframe === "weekly" ? "text-white" : "",
               )}
               onClick={() => setTimeframe("weekly")}
@@ -62,7 +62,7 @@ export default function Dashboard({ username }: DashboardProps) {
           <div>
             <button
               className={cn(
-                "text-white/40",
+                "text-white/40 transition-all duration-300 hover:text-white/80",
                 timeframe === "monthly" ? "text-white" : "",
               )}
               onClick={() => setTimeframe("monthly")}
@@ -86,11 +86,16 @@ export default function Dashboard({ username }: DashboardProps) {
           return (
             <section key={index} className="overflow-hidden rounded-[1.1rem]">
               {/* <div className={cn("-mt-[10px] px-4 pt-[48px]", testClass)}> */}
-              <div className={cn("rounded-b-[2rem] pt-[38px]", testClass)}>
-                <div className="rounded-t-[1.1rem] bg-tt-dark-blue px-6">
+              <div
+                className={cn(
+                  "cursor-pointer rounded-b-[2rem] pt-[36px]",
+                  testClass,
+                )}
+              >
+                <div className="rounded-t-[1.1rem] bg-tt-dark-blue px-6 transition-all duration-300 hover:bg-[#34397b]">
                   <div className="flex items-center justify-between pt-7">
                     <h2 className="font-bold">{item.title}</h2>
-                    <div className="relative h-[5px] w-[28px]">
+                    <div className="relative h-[5px] w-[28px] opacity-50 transition-all duration-200 hover:opacity-100">
                       <Image
                         src="./images/icon-ellipsis.svg"
                         className="object-contain"
